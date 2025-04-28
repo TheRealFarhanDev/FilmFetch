80eda68f2d59679b84e32548de02d330995d46aa
Mohammed Farhan <mohammedfarhan8612@gmail.com>
2025-04-28 19:54:56 +0530

    Update Card.jsx
    Ui improvement Fixed Extra space in specific Movie Cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = React.memo(
             <Link to={`/movie/${movie.id}`} className="block">
                 <div className='bg-[#0F0D23] rounded-lg text-white p-2.5 flex flex-col items-start cursor-pointer'>
                     <img className='rounded-lg object-cover w-full h-[350px]' src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : `/No-Poster-vertical.png`} alt="no poster" loading='lazy' />
-                    <h1 className='font-bold my-3 text-xl'>{movie.title}</h1>
+                    <h1 className='font-bold my-3 text-xl truncate'>{movie.title}</h1>
                     <div className='flex items-center gap-2 text-gray-400 text-lg'>
                         <div className='flex items-center gap-1'>
                             <img className='align-middle pb-0.5 w-4 h-4' src="/star.png" alt="star" />
